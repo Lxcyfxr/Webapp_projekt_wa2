643f558aa2d19eefc89557fd892f25aa170ab68f
ABCelias <eliaskunkel1@gmail.com>
2025-04-09 16:39:01 +0200

    register system anfang

--- public/login.php
+++ public/login.php
@@ -1,4 +1,44 @@
-<!DOCTYPE html>
+<?php
+    require("connection.php");
+
+
+    if(isset($_POST["submit"])){
+        var_dump($_POST);
+
+        $username = $_POST["username"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+
+
+        $stmt = $con->prepare("SELECT * FROM users WHERE username=:username OR email:=email");
+        $stmt->bindParam(":username", $username);
+        $stmt->bindParam(":email", $email);
+        $stmt->execute();
+
+
+        $userAlreadyExists = $stmt->fetchColumn();
+
+        if(!$userAlreadyExists){
+            //Registrieren
+            registerUser($username, $email, $password);
+            }
+        else{
+            //User exisitert schon
+        }
+
+        //User wird registriert
+        function registerUser($username, $email, $password){
+            global $con;
+            $stmt = $con->prepare("INSERT INTO users(username, email, password) VALUES(:username, :email, :password)");
+            $stmt->bindParam(":username", $username);
+            $stmt->bindParam(":email", $email);
+            $stmt->bindParam(":password", $password);
+            stmt->execute();
+
+        }
+    }
+
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -17,12 +57,12 @@
       <!--Login/Profil-->
       <li style="float: right"><a href="contact.html">Kontakt</a></li>
     </ul>
-    <form action="Post">
+    <form action="login.php" method="POST">
         <label for="username">Username:</label><br>
         <input type="text" id="username" name="username"><br><br>
         <label for="password">Password:</label><br>
         <input type="password" id="password" name="password"><br><br>
-        <input type="submit" value="Login">
+        <input type="submit" value="Login" name="submit>
     </form>
 </body>
 </html>

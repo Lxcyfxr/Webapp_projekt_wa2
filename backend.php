1c267f7cc2372569cd779d79cb7e746c0350f6fb
Lxc <128301314+Lxcyfxr@users.noreply.github.com>
2025-04-09 16:40:34 +0200

    backend.php für produkte

--- backend.php
+++ backend.php
@@ -1,16 +1,38 @@
-<!--dummie data-->
-
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "app_user";
+$password = "";
+$dbname = "webapp_project";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+// // SQL query
+// $sql = "SELECT * FROM products"; // Beispiel-Tabelle "users"
+// $result = $conn->query($sql);
+
+// $products = [];
+
+// if ($result->num_rows > 0) {
+//     while ($row = $result->fetch_assoc()) {
+//         $products[] = $row;
+//     }
+// }
+
+// header('Content-Type: application/json');
+// echo json_encode($products);
+
+// $conn->close();
+setcookie("username","Max",0);
 ?>
+<html>
+  <head></head>
+<body>
+  This site was moved permanently
+</body>
+</html>

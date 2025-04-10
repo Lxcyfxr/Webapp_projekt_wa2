5ba12996e636c377e0b1337b20f89ca6ff4a09a6
staufers18 <schedel.loris@gmail.com>
2025-04-10 15:22:46 +0200

    navbar css style edit, products index -> shopsite

--- public/index.php
+++ public/index.php
@@ -8,32 +8,6 @@
   <body style="background: #333; color: white">
     <?php include 'navbar.html'; ?>
     <h1 style="padding-top: 3%">Home Seite</h1>
-    <!-- test für produktintegration -->
-    <h2>Product List</h2>
-<ul id="product-list"></ul>
-    <script>
-      $(document).ready(function() {
-    console.log("jQuery is working!");
-});
-$(document).ready(function() {
-    $.ajax({
-        url: '../backend.php',
-        method: 'GET',
-        dataType: 'json',
-        success: function(data) {
-            data.forEach(function(product) {
-                $('#product-list').append(
-                    `<li>${product.name} - ${product.description}</li>`
-                );
-            });
-        },
-        error: function(xhr, status, error) {
-            console.error('Error fetching data:', error);
-        }
-    });
-});
-</script>
-    
     <footer>
       Platz für aktuelle Angebote, Rabatte, Aktionen. Hier sollen auch
       Neuerscheinungen und Bestseller angezeigt werden
--- public/shopsite.php
+++ public/shopsite.php
@@ -8,7 +8,31 @@
     <?php include 'navbar.html'; ?>
     <h1 style="padding-top: 3%">Shopseite</h1>
     <div width=90%>
-        
+      <!-- test für produktintegration -->
+      <h2>Product List</h2>
+      <ul id="product-list"></ul>
+      <script>
+          $(document).ready(function() {
+            console.log("jQuery is working!");
+          });
+          $(document).ready(function() {
+            $.ajax({
+              url: '../backend.php',
+              method: 'GET',
+              dataType: 'json',
+              success: function(data) {
+                  data.forEach(function(product) {
+                      $('#product-list').append(
+                          `<li>${product.name} - ${product.description}</li>`
+                      );
+                  });
+              },
+              error: function(xhr, status, error) {
+                  console.error('Error fetching data:', error);
+              }
+          });
+        });
+      </script>
     </div>
   </body>
 </html>

992a77442c3da85da58d3579843b1e18280a6d38
ABCelias <eliaskunkel1@gmail.com>
2025-04-23 17:07:54 +0200

    navbar einloggen passt

--- public/auth.php
+++ public/auth.php
@@ -90,9 +90,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="./authentication.css">
+    <link rel="stylesheet" href="./nav_bar.css"/>
     <title>Sign in & Sign up Form</title>
 </head>
 <body>
+<ul class="navbar">
+      <li class="navbarimg"><a href="index.php"><img src="pictures/Logo_Stylung-Photoroom(1).png" width="50" height="50"></a></li>
+      <li class="navbar"><a href="shopsite.php">Produkte</a></li>
+      <li class="navbar"><a href="shopsite.php?gender=MALE">Männer</a></li>
+      <li class="navbar"><a href="shopsite.php?gender=FEMALE">Frauen</a></li>
+      <li class="navbar" style="float: right">
+        <?php if (isset($_SESSION["username"])): ?>
+          <!-- Bild anzeigen wenn eingeloggt -->
+          <a href="auth.php"><img src="pictures/usericon.svg" alt="Profil" width="20" height="20"></a>
+        <?php else: ?>
+          <!-- Login Text wenn nicht eingeloggt -->
+          <a class="active" href="auth.php">Login</a>
+        <?php endif; ?>
+      </li>
+      <li class="navbar" style="float: right"><a href="contact.php">Kontakt</a></li>
+</ul>
 <div class="container">
     <div class="forms-container">
         <div class="signin-signup">

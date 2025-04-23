c587e6816a5525addf208ca5123f9fe1991f3720
staufers18 <schedel.loris@gmail.com>
2025-04-23 21:45:48 +0200

    Navbar Anpassung

--- public/navbar.php
+++ public/navbar.php
@@ -14,20 +14,20 @@
   </head>
   <body>
     <ul class="navbar">
-      <li class="navbarimg"><a href="index.php"><img src="pictures/Logo_Stylung-Photoroom(1).png" width="50" height="50"></a></li>
-      <li class="navbar"><a href="shopsite.php">Produkte</a></li>
-      <li class="navbar"><a href="shopsite.php?gender=MALE">Männer</a></li>
-      <li class="navbar"><a href="shopsite.php?gender=FEMALE">Frauen</a></li>
+      <li class="navbarimg"><a href="index.php"><img src="pictures/Logo_Stylung-Photoroom(1).png" width="50"></a></li>
+      <li class="navbar"><a class="classic" href="shopsite.php">Produkte</a></li>
+      <li class="navbar"><a class="classic" href="shopsite.php?gender=MALE">Männer</a></li>
+      <li class="navbar"><a class="classic" href="shopsite.php?gender=FEMALE">Frauen</a></li>
       <li class="navbar" style="float: right">
         <?php if (isset($_SESSION["username"])): ?>
           <!-- Bild anzeigen wenn eingeloggt -->
-          <a href="auth.php"><img src="pictures/usericon.svg" alt="Profil" width="19" height="19"></a>
+          <a class="active" href="auth.php"><img src="pictures/usericon.svg" alt="Profil" width="19" height="19"></a>
         <?php else: ?>
           <!-- Login Text wenn nicht eingeloggt -->
           <a class="active" href="auth.php">Login</a>
         <?php endif; ?>
       </li>
-      <li class="navbar" style="float: right"><a href="contact.php">Kontakt</a></li>
+      <li class="navbar" style="float: right"><a class="classic" href="contact.php">Kontakt</a></li>
     </ul>
   </body>
 </html>
